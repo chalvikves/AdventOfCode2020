d032ec7f837d6f7814b9b6a00e28694a9e76d3bb
chalvikves <vikves@student.chalmers.se>
2020-12-05 16:16:39 +0100

    Day 5 finished

---
+++ Day 5/secondStar.py
@@ -0,0 +1,47 @@
+def read_input():
+    with open(file = 'input.txt', mode = 'r') as f:
+        return f.read().split("\n")
+        
+def read_test_input():
+    with open(file = 'test.txt', mode = 'r') as f:
+        return f.read().split("\n")
+          
+inp = read_input()
+
+def findRow(letter, high, low):
+    mid = low + (high - low) // 2
+    if high == low:
+        return [low, high]
+    if letter in "F" or letter in 'L':
+        return [low, mid]
+    else:
+        return [mid+1, high]
+
+highest = []
+
+for i in inp:
+    temp = list(range(0, 128))
+    tema = list(range(0, 8))
+
+    for n in i:
+        if n == 'R' or n == 'L':
+            tema = findRow(n, max(tema), min(tema))
+        if n == 'F' or n == 'B':
+            temp = findRow(n, max(temp), min(temp))
+        
+    row = temp[0]
+    column = tema[0]
+    seatID = row * 8 + column
+    highest.append(seatID)
+    #print(f'row: {row}, column: {column}, seat ID: {seatID}')
+
+
+def findSeat(arr):
+    arr.sort()
+    
+    for i in range(0,len(arr)+1):
+       if arr[i+1] - arr[i] == 2:
+           return arr[i] + 1
+    
+
+print(findSeat(highest))
